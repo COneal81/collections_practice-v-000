e33e1caafe285b3a2e14f76f675d5dc785b79502
Kari O'Neal <triadfirewood@gmail.com>
2018-08-03 11:39:56 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -12,8 +12,8 @@ def sort_array_desc(array)
 end
 
 def sort_array_char_count(array)
-    array.sort do |a, b| array.length(0..-1)
-    a<=>b
+    array.sort do |0, -1| array.length
+    0<=>-1
   end
 end
   
