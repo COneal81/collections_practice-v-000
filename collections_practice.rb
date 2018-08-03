3d0e796cdcc81ad328a3b8eb0d7b8db1104cbc1f
Kari O'Neal <triadfirewood@gmail.com>
2018-08-03 12:30:40 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,7 +1,7 @@
 def sort_array_asc(array)
   array.sort do |a, b|
     a<=>b
-end
+  end
 end
 
 
@@ -11,19 +11,34 @@ def sort_array_desc(array)
   end
 end
 
+
 def sort_array_char_count(array)
     array.sort do |left, right|
     left.length<=>right.length
   end
 end
 
+
 def swap_elements(array)
   array[1], array[2] = array[2], array [1]
   array
 end  
 
+
 def reverse_array(array)
   array.reverse do |elements|array
-end  
+  end  
 end
+  
+
+def kesha_maker(array)
+  
+  
+  
+  
+  
+  
+  
+  
+  
   
